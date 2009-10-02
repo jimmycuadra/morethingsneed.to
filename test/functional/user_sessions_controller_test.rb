b2c6c55d1316ae066e78eeb5c013f54b875499be
Jimmy Cuadra <github@jimmycuadra.com>
2009-10-02 04:47:56 +0000

    Added functional tests for user_sessions and updated users fixtures.

---
+++ test/functional/user_sessions_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserSessionsControllerTest < ActionController::TestCase
+  fixtures :users
+  
+  test "should log user in and redirect to root with message" do
+    post :create, :user_session => { :username => 'Wongo', :password => 'pass' }
+    assert_redirected_to root_path
+    assert_not_nil flash[:notice]
+  end
+  
+  test "should render new template on bad login" do
+    post :create
+    assert_template :new
+  end
+end
