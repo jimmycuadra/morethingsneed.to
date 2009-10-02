b3bb447c50f467acb8978522d1df293736445be2
Jimmy Cuadra <github@jimmycuadra.com>
2009-10-02 03:12:57 +0000

    Adding tests for entries controller, removing check for post request in create action.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -11,20 +11,16 @@ class EntriesController < ApplicationController
   end
   
   def create
-    if request.post?
-      @new_entry = Entry.new(params[:entry])
-      @new_entry.ip = request.remote_ip
-      @new_entry.user_id = current_user.id if current_user
-      if @new_entry.save
-        flash[:success] = 'More submissions need to be successful.'
-        redirect_to @new_entry
-      else
-        flash[:error] = 'More submissions need to be filled out correctly.'
-        @entries = Entry.paginate :page => params[:page], :order => 'created_at DESC'
-        render :index
-      end
+    @new_entry = Entry.new(params[:entry])
+    @new_entry.ip = request.remote_ip
+    @new_entry.user_id = current_user.id if current_user
+    if @new_entry.save
+      flash[:success] = 'More submissions need to be successful.'
+      redirect_to @new_entry
     else
-      redirect_to :back
+      flash[:error] = 'More submissions need to be filled out correctly.'
+      @entries = Entry.paginate :page => params[:page], :order => 'created_at DESC'
+      render :index
     end
   end
   
