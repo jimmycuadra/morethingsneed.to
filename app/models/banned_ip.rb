95e07c2f0a0e353c59da51de954f2827f47ac894
Jimmy Cuadra <github@jimmycuadra.com>
2010-01-30 15:28:14 -0800

    adds is_banned? and toggle_ban methods to BannedIp, adds tests - refs #20

--- app/models/banned_ip.rb
+++ app/models/banned_ip.rb
@@ -2,4 +2,18 @@ class BannedIp < ActiveRecord::Base
   # validations
   
   validates_format_of :ip, :with => /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/, :message => 'must be a valid IP'
+  
+  # methods
+  
+  def self.is_banned?(ip)
+    find_by_ip(ip)
+  end
+  
+  def self.toggle_ban(ip)
+    if banned_record = (is_banned? ip)
+      destroy banned_record
+    else
+      create(:ip => ip)
+    end
+  end  
 end
--- test/unit/banned_ip_test.rb
+++ test/unit/banned_ip_test.rb
@@ -10,4 +10,24 @@ class BannedIpTest < ActiveSupport::TestCase
     assert !@b.valid?
     assert @b.errors.invalid?(:ip)
   end
+  
+  test "should not return banned ip record" do
+    assert_nil BannedIp.is_banned?('0.0.0.0')
+  end
+  
+  test "should return banned ip record" do
+    assert_not_nil BannedIp.is_banned?('255.255.255.255')
+  end
+  
+  test "should ban ip" do
+    assert_difference 'BannedIp.count', 1 do
+      BannedIp.toggle_ban('0.0.0.0')
+    end
+  end
+  
+  test "should unban ip" do
+    assert_difference 'BannedIp.count', -1 do
+      BannedIp.toggle_ban('255.255.255.255')
+    end
+  end
 end
