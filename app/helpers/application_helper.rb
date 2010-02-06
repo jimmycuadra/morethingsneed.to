7678e72f7da13dd139608277a003686a7c758a4d
Jimmy Cuadra <github@jimmycuadra.com>
2010-02-06 15:12:02 -0800

    updates display of datetime strings - closes #42
    adds helper methods to strip leading zeroes from each datetime format
    uses new helper methods in entry and comment partials
    removes unused datetime format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,13 @@ module ApplicationHelper
   def render_error_messages(*objects)
     messages = objects.compact.map { |o| o.errors.full_messages }.flatten
     render :partial => 'partials/error_messages', :object => messages unless messages.empty?
-  end  
+  end
+  
+  def datetime_written_out(record)
+    record.created_at.to_s(:written_out).gsub(/([^\d:])0(\d+)/, '\1\2')
+  end
+  
+  def datetime_simple(record)
+    record.created_at.to_s(:simple).gsub(/0?(\d+)\/0?(\d+)/, '\1/\2')
+  end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,2 @@
-Time::DATE_FORMATS[:datetime] = '%Y-%m-%d'
-Time::DATE_FORMATS[:written_out] = '%A, %B %d, %Y at %l:%M:%S %p'
-Time::DATE_FORMATS[:simple] = '%m/%d/%y at %l:%M:%S %p'
+Time::DATE_FORMATS[:written_out] = '%A, %B %d, %Y at %l:%M %p'
+Time::DATE_FORMATS[:simple] = '%m/%d/%y at %l:%M %p'
