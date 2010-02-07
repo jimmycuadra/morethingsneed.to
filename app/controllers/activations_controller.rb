9c47d92f276136dbc712d3250e98583c12d38a97
Jimmy Cuadra <github@jimmycuadra.com>
2010-02-06 23:28:09 -0800

    adds feature to resend activation email - closes #34
    updates ActivationsController and associated views to
    allow resending activation email similar to the
    password reset feature
    
    moves old create action to new activate action,
    create action now generates a new activation email
    
    routes updated for new actions
    
    cleans up activation emails
    
    adds link to new activation email form on login page/userbox

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -1,5 +1,22 @@
 class ActivationsController < ApplicationController
+  def new
+  end
+  
   def create
+    @user = User.find_by_email(params[:email])
+    if @user
+      @user.reset_perishable_token!
+      ActionMailer::Base.default_url_options[:host] = request.host
+      Notifier.deliver_activation_instructions(@user)
+      flash[:success] = 'Please check your e-mail for a new copy of your activation instructions.'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Sure you got that e-mail address right? We don\'t have an account registered to it.'
+      render :action => :new
+    end
+  end
+  
+  def activate
     @user = User.find_using_perishable_token(params[:activation_code], 1.week) || (raise ActiveRecord::RecordNotFound)
     if @user.active?
       flash[:error] = 'Your account is already active.'
@@ -13,5 +30,5 @@ class ActivationsController < ApplicationController
       end
     end    
     redirect_to root_path
-  end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.register '/register', :controller => :users, :action => 'new'
   map.profile '/profile', :controller => :users, :action => 'edit'
-  map.activate '/activate/:activation_code', :controller => 'activations', :action => 'create'
+  map.resources :activations, :only => [:new, :create]
+  map.activate '/activate/:activation_code', :controller => 'activations', :action => 'activate'
   map.resources :password_resets, :only => [:new, :edit, :create, :update]
 
   map.login '/login', :controller => 'user_sessions', :action => 'new'
