d3e9a9d0d0c5f1f5924ec4c77103bad15e4a3069
Jimmy Cuadra <github@jimmycuadra.com>
2010-02-08 00:06:10 -0800

    fixes bug in form defaults after posting an entry - closes #45

--- public/javascripts/entry.js
+++ public/javascripts/entry.js
@@ -19,7 +19,7 @@ MTNT.Entry.prototype.handleSubmit = function(e, form) {
 MTNT.Entry.prototype.handleResponse = function(response) {
   if (response.success) {
     this.$form[0].reset();
-    this.$form.formDefaults();
+    this.$form.find('input[type=text]').css('color', '#666');
     this.flash.update(response.success, response.message);
     this.insert(response.entry);
   } else {
