8a2a10179f554a0c904d3223ed8d02e6e1f38fb7
Jimmy Cuadra <github@jimmycuadra.com>
2010-02-07 19:34:27 -0800

    updates seeds for multiple environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
-def random_spam
-  rand(5) < 2 ? true : false
-end
-
-ipm = IP_Maker.new
-
-1.upto(20) do |i|
-  e = Entry.new(:noun => i.to_s, :verb => i.to_s)
-  e.ip = ipm.generate
-  e.spam = random_spam
-  e.save
-  rand(10).times do
-    c = e.comments.build(:comment => 'Comment')
-    c.ip = ipm.generate
-    c.spam = random_spam
-    c.save
-  end
-  rand(15).times do
-    v = e.votes.build(:up_vote => true)
-    v.ip = ipm.generate
-    v.save
-  end
-  rand(15).times do
-    v = e.votes.build(:up_vote => false)
-    v.ip = ipm.generate
-    v.save
-  end
-end
-
-u = User.new(:password => 'asdf', :password_confirmation => 'asdf', :email => 'jimmy@morethingsneed.to')
+u = User.new(:password => APP_CONFIG['jimmy_default_password'], :password_confirmation => APP_CONFIG['jimmy_default_password'], :email => APP_CONFIG['jimmy_email'])
 u.username = 'Jimmy'
 u.admin = true
 u.active = true
 u.save
 
-u = User.new(:password => 'asdf', :password_confirmation => 'asdf', :email => 'mike@morethingsneed.to')
-u.username = 'Mike'
+u = User.new(:password => APP_CONFIG['mike_default_password'], :password_confirmation => APP_CONFIG['mike_default_password'], :email => APP_CONFIG['mike_email'])
+u.username = 'nebyoolae'
 u.admin = true
-u.active = true
-u.save
-
-u = User.new(:password => 'asdf', :password_confirmation => 'asdf', :email => 'guest@website.com')
-u.username = 'Guest'
 u.active = true
 u.save
