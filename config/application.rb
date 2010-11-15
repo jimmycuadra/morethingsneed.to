749c9f145a43685b4f128faf6b4996fe79e4516d
Jimmy Cuadra <jimmy@jimmycuadra.com>
2010-11-15 00:09:24 -0800

    switch APP_CONFIG to ENV for heroku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'yaml'
-APP_CONFIG = YAML.load(File.read(File.expand_path('../app_config.yml', __FILE__)))
 require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
