eea218e8a3364ba7e002cba263dcb7231cb5eb37
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-02-02 20:28:58 -0800

    remove site5-specific gem paths

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,3 @@
-if File.exists?('/home/jimmycua')
-  ENV["GEM_HOME"]="/home/jimmycua/ruby/gems"
-  ENV["GEM_PATH"]="/home/jimmycua/ruby/gems:/usr/lib/ruby/gems/1.8"
-end
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 require File.join(File.dirname(__FILE__), '/../lib/ip_maker')
