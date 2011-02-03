7250563e1efa8547296e3130dc89c7cb2e351762
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-02-02 21:04:53 -0800

    don't require ip_maker from environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require File.join(File.dirname(__FILE__), '/../lib/ip_maker')
 
 # Initialize the rails application
 Morethingsneed::Application.initialize!
