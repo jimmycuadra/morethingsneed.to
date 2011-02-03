62d4cdf73be020e39dd1f2e2ea0ade373843843f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-02-02 22:11:43 -0800

    change secret token and load it from env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Morethingsneed::Application.config.secret_token = '4aa23456d1dc4153635e3f37b43e98b084d21a18f4bcfef8f10f09661d3ab26bfef21835e7f7f2a30ad5d563d491949ba8fb70042e2f1a67a6218e982b695711'
+Morethingsneed::Application.config.secret_token = ENV['SECRET_TOKEN']
