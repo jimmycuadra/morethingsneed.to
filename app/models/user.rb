03c4e77c2b2958f10e6c6fe14f16b04bd8c175fa
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-10-16 00:16:17 -0700

    Test and refactor creation of users via auth hash.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,29 @@ class User < ActiveRecord::Base
   private
 
   def self.create_via_auth_hash(auth_hash)
-    user = User.create(name: auth_hash["info"]["name"], email: auth_hash["info"]["email"])
-    user.authentications.create!(
-      provider: auth_hash["provider"],
-      uid: auth_hash["uid"],
-      token: auth_hash["credentials"]["token"],
-      secret: auth_hash["credentials"]["secret"]
-    )
+    user = populate_user_from_auth_hash(auth_hash)
+    create_authentication_for_user_from_auth_hash(user, auth_hash)
     user
+  end
+
+  def self.populate_user_from_auth_hash(auth_hash)
+    user_params = { name: auth_hash["info"]["name"] }
+    user_params[:email] = auth_hash["info"]["email"] if auth_hash["info"]
+
+    User.create! user_params
+  end
+
+  def self.create_authentication_for_user_from_auth_hash(user, auth_hash)
+    auth_params = {
+      provider: auth_hash["provider"],
+      uid: auth_hash["uid"]
+    }
+
+    if auth_hash["credentials"]
+      auth_params[:token] = auth_hash["credentials"]["token"]
+      auth_params[:secret] = auth_hash["credentials"]["secret"]
+    end
+
+    user.authentications.create! auth_params
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,15 +11,30 @@ describe User do
   end
 
   describe ".find_or_create_via_auth_hash" do
+    let(:auth_hash) do
+      {
+        "provider" => "test",
+        "uid" => "1",
+        "info" => {
+          "name" => "Bongo",
+          "email" => "bongo@example.com"
+        },
+        "credentials" => {
+          "token" => SecureRandom.hex(8),
+          "secret" => SecureRandom.hex(8)
+        }
+      }
+    end
+
     it "finds existing users" do
       auth = Factory(:authentication)
-
-      auth_hash = {
-        "provider" => "test",
-        "uid" => "1"
-      }
-
       User.find_or_create_via_auth_hash(auth_hash).should == auth.user
+    end
+
+    it "creates a new user when no user is found" do
+      expect {
+        User.find_or_create_via_auth_hash(auth_hash)
+      }.to change { User.count }.from(0).to(1)
     end
   end
 end
