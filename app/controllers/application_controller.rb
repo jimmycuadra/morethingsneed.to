3789c5fe642be973cb0d24284c7c73e2f9cd43a1
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-10-15 18:44:46 -0700

    Extract layout into partials. Memoize current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
 
   def current_user
-    User.find(session[:user_id]) if logged_in?
+    @current_user ||= User.find(session[:user_id]) if logged_in?
   end
   helper_method :current_user
 end
