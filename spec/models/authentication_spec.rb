44807c131a5adeb7a3f2c8b4463a4e5b54100d60
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-10-16 22:42:31 -0700

    Test that authentications belong to a user. Reword presence validation tests for User.

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe Authentication do
   ["provider", "uid", "user_id"].each do |attribute|
-    it "requires a #{attribute}" do
+    it "requires #{attribute} attribute" do
       subject.valid?
       subject.errors[attribute.to_sym].should_not be_empty
     end
+  end
+
+  it "belongs to a user" do
+    authentication = Factory(:authentication)
+    authentication.user.should be
   end
 end
