9be54f667b78385526889be91e1d934b34be5f8b
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-10-16 22:01:49 -0700

    Add association and custom uniqueness validation to Entry.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -7,4 +7,12 @@ class Entry < ActiveRecord::Base
   validates :verb, presence: true
   validates :needs, inclusion: { in: [true, false] }
   validates :user_id, presence: true
+  validate  :uniqueness_of_entry
+
+  def uniqueness_of_entry
+    matched_entry = Entry.where(['LOWER(noun) = LOWER(?) AND LOWER(verb) = LOWER(?)', noun, verb]).first
+    if matched_entry && (matched_entry.id != id)
+      errors.add(:base, 'Entry is not unique.')
+    end
+  end
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -7,4 +7,15 @@ describe Entry do
       subject.errors[attribute.to_sym].should_not be_empty
     end
   end
+
+  it "belongs to a user" do
+    Factory(:entry).user.should be
+  end
+
+  it "must have a unique noun/verb combination" do
+    Factory(:entry)
+    entry = Factory.build(:entry)
+    entry.valid?
+    entry.errors[:base].should_not be_empty
+  end
 end
