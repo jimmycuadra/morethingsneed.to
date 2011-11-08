dc30d41c1eb875fff94c440a257d4ff4128d6373
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-11-07 23:19:59 -0800

    Add Google strategy for OmniAuth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"]
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], {
+    scope: "https://www.googleapis.com/auth/plus.me"
+  }
 end
