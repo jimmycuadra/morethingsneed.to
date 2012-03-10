4b5fbfd1d38209d5005fbb77a630d4368a226eca
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-03-10 01:57:56 -0800

    Add attr_accessible and create action for entries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,6 @@
 class EntriesController < ApplicationController
+  respond_to :json
+
   def index
     @entries = Entry.order("created_at DESC")
 
@@ -6,5 +8,9 @@ class EntriesController < ApplicationController
       format.html
       format.json { render json: @entries }
     end
+  end
+
+  def create
+    respond_with Entry.create(params[:entry])
   end
 end
