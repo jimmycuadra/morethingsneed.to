c88087f57ed799cafe3b36cc4d1b123d8fc0a0de
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-05 14:40:31 -0800

    Add ability to log out.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ MorethingsneedTo::Application.routes.draw do
 
   match "auth/:provider/callback", to: "authentications#create"
   match "auth/failure", to: redirect("/")
+  match "logout", to: "sessions#destroy", as: :log_out
 
   root to: "entries#index"
 end
