1774ab5a3a088fc3588a18cbb8b18c653f7322cf
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-05 14:19:14 -0800

    Add auth/failure route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 MorethingsneedTo::Application.routes.draw do
   resources :entries
 
-  match "auth/:provider/callback" => "authentications#create"
+  match "auth/:provider/callback", to: "authentications#create"
+  match "auth/failure", to: redirect("/")
 
   root to: "entries#index"
 end
