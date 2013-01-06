814361dec64f43aefb8e71eeefc1ded8d86a3b25
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-05 20:52:51 -0800

    Use Rails logger for OmniAuth logging.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@ Rails.configuration.middleware.use(OmniAuth::Builder) do
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
   provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
 end
+
+OmniAuth.config.logger = Rails.logger
