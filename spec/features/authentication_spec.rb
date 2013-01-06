c8b742da925739fea9777857fbc42cf394b071a3
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-06 11:15:15 -0800

    Add scenario for changing user name.

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -42,4 +42,16 @@ feature "Authentication" do
     click_button "Log in"
     expect(page).to have_content("Log out")
   end
+
+  scenario "changing user name" do
+    visit root_path
+    click_on "Log in with Facebook"
+    click_on "Bongo"
+    fill_in "user_name", with: ""
+    click_on "Update"
+    expect(page).to have_content("Name can't be blank")
+    fill_in "user_name", with: "Max Measles"
+    click_on "Update"
+    expect(page).to have_content("Max Measles")
+  end
 end
