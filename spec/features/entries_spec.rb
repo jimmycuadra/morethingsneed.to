25355ff120fe79ddf3245f7591dde950d5d7caff
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-06 11:11:20 -0800

    Enhance entries feature spec.

--- spec/features/entries_spec.rb
+++ spec/features/entries_spec.rb
@@ -1,13 +1,21 @@
 require "spec_helper"
 
 feature "Entries", js: true do
-  scenario "adding a new entry" do
+  scenario "adding new entries" do
     visit root_path
     click_on "Log in with Facebook"
+    click_on "This needs to happen" # Submit without filling in the form.
+    expect(page).to have_content("Noun can't be blank")
     fill_in "entry_noun", with: "foo"
     select "needs", from: "entry_needs"
     fill_in "entry_verb", with: "bar"
     click_on "This needs to happen"
     expect(page).to have_content("More foo needs to bar.")
+    expect(find("#entry_noun").value).to be_empty
+    fill_in "entry_noun", with: "pugs"
+    select "need", from: "entry_needs"
+    fill_in "entry_verb", with: "buzz"
+    click_on "This needs to happen"
+    expect(page).to have_content("More pugs need to buzz.")
   end
 end
