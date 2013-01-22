1525879b1a020be7e711a508e8441088490f3b73
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-22 01:14:00 -0800

    Improve test coverage for Authentication.

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -53,4 +53,77 @@ describe Authentication do
       end
     end
   end
+
+  describe ".from_persona" do
+    let(:authentication) do
+      described_class.new(provider: "persona", uid: "bongo@example.com")
+    end
+
+    let(:json) do
+      MultiJson.dump(status: "okay", email: "bongo@example.com")
+    end
+
+    let(:response) do
+      response = double("response")
+      response.stub(:body) { json }
+      response
+    end
+
+    before do
+      Faraday.stub(:post) { response }
+    end
+
+    context "with a returning user" do
+      let(:relation) do
+        relation = double("relation")
+        relation.stub(:first) { authentication }
+        relation
+      end
+
+      before do
+        described_class.stub(:where) { relation }
+      end
+
+      it "finds the matching authentication" do
+        expect(described_class.from_persona("assertion")).to eql(authentication)
+      end
+    end
+
+    context "with a new user" do
+      let(:relation) do
+        relation = double("relation")
+        relation.stub(:first) { nil }
+        relation
+      end
+
+      before do
+        described_class.stub(:where) { relation }
+      end
+
+      it "creates a new user" do
+        expect {
+          described_class.from_persona("assertion")
+        }.to change {
+          User.count
+        }.by(1)
+      end
+
+      it "returns the new authentication" do
+        authentication = described_class.from_persona("assertion")
+        expect(authentication).to be_persisted
+      end
+    end
+
+    context "when the assertion verification fails" do
+      let(:json) do
+        MultiJson.dump(status: "failure")
+      end
+
+      it "raises an error" do
+        expect {
+          described_class.from_persona("assertion")
+        }.to raise_error(Authentication::PersonaAssertionError)
+      end
+    end
+  end
 end
