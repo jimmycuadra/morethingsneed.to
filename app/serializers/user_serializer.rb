398a73989e2cc597ac313b167d316d7e947425df
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-01-17 01:35:26 -0800

    Don't serialize associations with User for now.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,18 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  embed :ids, include: true
-
   attributes *%i{
     username
   }
-
-  has_many :comments
-  has_many :entries
-
-  def comments
-    object.comments.where(spam: false)
-  end
-
-  def entries
-    object.entries.where(spam: false)
-  end
 end
